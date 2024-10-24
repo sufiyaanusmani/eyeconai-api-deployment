4f195a8ded7ed9f0df5a9af484a9a6e13cb8fcf4
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2024-10-24 18:51:53 +0500

    Set up app

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+    }
+);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
