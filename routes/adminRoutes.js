074fdab13bf68eacf145347ce7a13598d2008d9a
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2025-03-02 21:04:33 +0500

    Add organization retrieval and max cameras update functionality

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const { authorizeRole } = require('../middlewares/authMiddleware');
 const { createOrganization, createOrganizationAdmin, updateOrganization } = require('../controllers/organizationController');
-const { getAllOrganizations } = require('../controllers/adminController');
+const { 
+    getAllOrganizations, 
+    getOrganizationById, 
+    updateMaxCameras 
+} = require('../controllers/adminController');
 const ROLES = require("../constants/roles");
 
 const router = express.Router();
 
 // Organization routes
 router.get("/organizations", authorizeRole(ROLES.ADMIN), getAllOrganizations);
+router.get("/organization/:orgId", authorizeRole(ROLES.ADMIN), getOrganizationById);
 router.post("/organization", authorizeRole(ROLES.ADMIN), createOrganization);
 router.post("/organization/:orgId/admin", authorizeRole(ROLES.ADMIN), createOrganizationAdmin);
 router.put("/organization/:orgId", authorizeRole(ROLES.ADMIN), updateOrganization);
+router.patch("/organization/:orgId/max-cameras", authorizeRole(ROLES.ADMIN), updateMaxCameras);
 
 module.exports = router;
